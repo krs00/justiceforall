e8295ba3b1e4393ee442e0af9ebef0afe8601c7b
Kymani Stephens <kymanist00@gmail.com>
2023-11-16 23:12:14 -0800

    add mobile header

---
+++ src/components/pageheader/PageHeader.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import './PageHeader.css'; // Import the stylesheet
+
+const PageHeader = ({ }) => {
+  return (
+  <>
+    <header className='mobile-header'>
+      <nav>
+
+      </nav>
+    </header>
+  </>
+  )
+}
+
+export default PageHeader
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,18 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from "react";
+import Layout from "../components/layout";
+import PageHeader from "../components/pageheader/PageHeader";
 // import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
-  return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      {/* <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      /> */}
-      <p>test</p>
-      <p>test</p>
-      <p>test</p> 
-    </Layout>
-  )
-}
+  return  (
+    <>
+    <PageHeader>
 
-export const Head = () => <title>justiceforall</title>
+    </PageHeader>
+    </>
+    )
+};
 
-export default IndexPage
+export const Head = () => <title>justiceforall</title>;
+
+export default IndexPage;
