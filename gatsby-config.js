c99d6ce5354198e93d4637f1c725fab1e80d92fa
Kymani Stephens <kymanist00@gmail.com>
2023-11-16 16:32:30 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `justiceforall`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
