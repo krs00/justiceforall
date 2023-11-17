93aac8fac9668d1de37d7a76305f63cb069d6ab5
Kymani Stephens <kymanist00@gmail.com>
2023-11-16 17:28:08 -0800

    finish gatsby tutorial for images

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `justiceforall`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ], 
 }
